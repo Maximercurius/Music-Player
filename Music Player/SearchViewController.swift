6217dff0fe15c67d02d8e18ca13dc679ec4b12d7
Maxim <swiftdevfromsiberia@gmail.com>
2022-05-24 16:28:58 +0500

    Alamofire added with Xcode Packages. Cocoa pods are causing Ruby issue

--- Music Player/SearchViewController.swift
+++ Music Player/SearchViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 struct TrackModel {
     var trackName:String
@@ -46,6 +47,17 @@ class SearchViewController: UITableViewController {
 
 extension SearchViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        print(searchText)
+     //   print(searchText)
+        let url = "https://itunes.apple.com/search?term=\(searchText)"
+        
+        AF.request(url).responseData{ (dataResponse) in
+            if let error = dataResponse.error {
+                print ("Error recieved requesting data: \(error.localizedDescription)")
+                return
+            }
+            guard let data = dataResponse.data else { return }
+            let someString = String(data: data, encoding: .utf8)
+            print (someString ?? "")
+        }
     }
 }
