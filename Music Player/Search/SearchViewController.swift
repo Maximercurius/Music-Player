5079acd1a44cc4f87a63c4853236e6c2333b2cea
Maxim <swiftdevfromsiberia@gmail.com>
2022-06-06 08:54:39 +0500

    Track Details View is added

--- Music Player/Search/SearchViewController.swift
+++ Music Player/Search/SearchViewController.swift
@@ -102,6 +102,16 @@ func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> U
     cell.set(viewModel: cellViewModel)
     return cell
 }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cellViewModel = searchViewModel.cells[indexPath.row]
+        print("cellViewModel.trackName", cellViewModel.trackName)
+        
+        let window = UIApplication.shared.keyWindow
+        let trackDetailsView = Bundle.main.loadNibNamed("TrackDetailView", owner: self, options: nil)?.first as! TrackDetailView
+        window?.addSubview(trackDetailsView)
+    }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 84
     }
---
+++ Music Player/Search/TrackDetailView/TrackDetailView.swift
@@ -0,0 +1,14 @@
+//
+//  TrackDetailView.swift
+//  Music Player
+//
+//  Created by Makarov_Maxim on 06.06.2022.
+//
+
+import UIKit
+
+class TrackDetailView: UIView {
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
